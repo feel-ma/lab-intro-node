4367f2fe693b4df5d8cf50b01b6c8a9b9cde804b
Filippo Marinolli <f.marinolli@gmail.com>
2023-03-25 01:43:22 +0100

    not working

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items= []
+    this.length=this.items.length
+  }
 
-  add(item) {}
+  add(item) {this.items.push(item)}
 
-  get(pos) {}
+  get(pos) {
+    if(pos<this.items.length) return this.items[pos]
+    else throw new Error('OutOfBounds');
+  }
 
-  max() {}
+  max() {
+    if (this.items.length>0) return this.items[this.items.length-1]
+    else throw new Error('EmptySortedList');
+  }
 
-  min() {}
+  min() {
+    if (this.items.length>0) return this.items[0]
+    else throw new Error('EmptySortedList');
+  }
 
-  sum() {}
+  sum() {
+    let sum=0
+    for(let i=0; i<this.items.lenght; i++){
+      sum+=this.items[i]
+    }
+    return sum
+  }
 
-  avg() {}
+  avg() {
+    return (this.sum()/this.items.length)
+    
+  }
 }
 
 module.exports = SortedList;
